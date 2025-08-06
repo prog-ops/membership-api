849a1c2d7a40d23449de410393d4cad2fa2e5022
prog-ops <akunbaru14102019@gmail.com>
2025-08-06 20:32:01 +0700

    Add articles to sidebar

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -13,6 +13,11 @@ const mainNavItems: NavItem[] = [
         href: '/dashboard',
         icon: LayoutGrid,
     },
+    {
+        title: 'Articles',
+        href: '/articles',
+        icon: LayoutGrid,
+    },
 ];
 
 const footerNavItems: NavItem[] = [
