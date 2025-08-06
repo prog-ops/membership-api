bac72df0111c739ba921d210a95dccbd4d30773e
prog-ops <akunbaru14102019@gmail.com>
2025-08-06 20:02:36 +0700

    Add Article model

---
+++ app/Models/Article.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    use HasFactory; // Sebaiknya tambahkan ini juga
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'content',
+    ];
+}
