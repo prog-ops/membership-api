54ac00056848f70be924d57119919e5921c21ac4
prog-ops <akunbaru14102019@gmail.com>
2025-08-06 20:32:34 +0700

    Modify User interface and PageProps type

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -40,4 +40,11 @@ export interface User {
     created_at: string;
     updated_at: string;
     [key: string]: unknown; // This allows for additional properties...
+    membership_type: 'A' | 'B' | 'C';
 }
+
+export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
+    auth: {
+        user: User;
+    };
+};
